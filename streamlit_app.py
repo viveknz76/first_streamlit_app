19a892a2cab3ae82e3e14dfbc2dfcd4e3d638373
viveknz76 <viveknz76@gmail.com>
2023-07-03 11:52:28 +1200

    Update streamlit_app.py
    More text added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
